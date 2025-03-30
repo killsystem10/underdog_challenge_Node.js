916d8f528bfc87948f6ae8ca243f9833ee2ad6b3
killsystem10 <killsystem10@outlook.com>
2025-03-30 22:17:27 +0900

    Section 2_09 라우팅

--- basics/04/server-5.js
+++ basics/04/server-5.js
@@ -1,1 +1,19 @@
 // 라우팅 연습하기  (결과 비교 파일 : 04\results\server-5.js)
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    res.setHeader('content-type', 'text/plain');
+    const {url,method} = req;
+    if (method === 'get' && url==='/home') {
+        res.write('home');
+        res.end();
+    } else if (method === 'get' && url==='/about') {
+        res.end('about');
+    } else {
+        res.end('not found');
+    }
+})
+
+server.listen(3000, () => {
+    console.log('server running');
+})
