8dee3a7ac8fe88b269595ae535724d1890d6879f
killsystem10 <killsystem10@outlook.com>
2025-03-30 15:04:56 +0900

    Section 2_06. 노드의 path모듈

--- basics/03/path.js
+++ basics/03/path.js
@@ -1,2 +1,11 @@
 // path 모듈 연습하기 ( 결과 비교 파일 : 03\results\path.js)
+const path = require('path');
 
+const fullPath = path.join('some','work', 'ex.txt');
+console.log(fullPath);
+
+const dir = path.dirname(fullPath);
+console.log(dir);
+
+const name = path.basename(__filename);
+console.log(name);
