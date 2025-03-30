6e7c7518c2f6aa861dbd091c7bbabe893fd950f0
killsystem10 <killsystem10@outlook.com>
2025-03-30 23:04:25 +0900

    Section 3 02. Express 라우팅

--- myContacts/app.js
+++ myContacts/app.js
@@ -3,9 +3,21 @@ const app = express();
 app.get("/", (req,res) => {
     res.send("hello killsystem10");
 })
-app.get("/contacts", (req,res) => {
-    res.send("contacts page");
+app.get("/contacts/:id", (req,res) => {
+    res.send("view Contact for ID :", req.params.id);
 })
+app.post("/contacts/:id", (req,res) => {
+    res.send('create contacts',req.params.id);
+})
+
+app.put("contacts/:id", (req,res) => {
+    res.send(`Update contact for ID : ${req.params.id}`);
+})
+
+app.delete("contacts/:id", (req,res) => {
+    res.send(`Delete contact for ID : ${req.params.id}`);
+})
+
 app.listen(3000, () => {
     console.log('server running');
 })
