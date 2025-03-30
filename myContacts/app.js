3291fa9d1b51eecf5536e73184d3174bb1f904f3
killsystem10 <killsystem10@outlook.com>
2025-03-30 23:53:50 +0900

    Section 3 04. 바디파서 미들웨어

--- myContacts/app.js
+++ myContacts/app.js
@@ -26,6 +26,7 @@ app.delete("contacts/:id", (req,res) => {
 })
 */
 app.use("/contacts", require("./routes/contactRoutes"));
+app.use(express.urlencoded({ extended: true }));
 app.listen(3000, () => {
     console.log('server running');
 })
--- myContacts/routes/contactRoutes.js
+++ myContacts/routes/contactRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.route("/:id").get((req,res) => {
     res.send("view contact for id : " + req.params.id);
 }).post((req,res) => {
+    console.log(req.body);
     res.send("create contact for id : " + req.params.id);
 }).put((req,res) => {
     res.send("update contact for id : " + req.params.id);
