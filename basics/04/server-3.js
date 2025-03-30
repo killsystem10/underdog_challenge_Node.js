6e703229d18ee1430cd46b809501195c6ed25ff9
killsystem10 <killsystem10@outlook.com>
2025-03-30 22:07:04 +0900

    Section 2_08. HTTP모듈로 서버 만들기

--- basics/04/server-3.js
+++ basics/04/server-3.js
@@ -1,1 +1,12 @@
 // 응답 객체 확인하기 - 응답 헤더, 응답 본문, 응답 종료  (결과 비교 파일 : 04\results\server-3.js)
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    res.setHeader('content-type', 'text/plain');
+    res.write("hello killsystem");
+    res.end();
+})
+
+server.listen(3000, () => {
+    console.log('server running');
+})
