51d0e1a87ccd130379e6000a2a41b4c2d00e5f93
killsystem10 <killsystem10@outlook.com>
2025-03-30 14:25:06 +0900

    Section 2_03. 자바스크립트 기초 문법(2)

--- basics/02/sync.js
+++ basics/02/sync.js
@@ -1,1 +1,17 @@
 // 순서대로 실행하기  (결과 비교: 02\results\sync.js)
+let A = function() {
+    console.log("A");
+}
+let B = function(callback) {
+    setTimeout(() => {
+        console.log("B");
+        callback();
+    },2000)
+
+}
+let C = function() {
+    console.log("C");
+}
+
+A();
+B(C);
