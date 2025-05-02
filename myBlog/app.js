40b6c8e7ef8e7345bcb93485822e52ede2252bee
killsystem10 <killsystem10@outlook.com>
2025-05-03 02:33:00 +0900

    Section 5 01. 블로그 앱 만들기 - 레이아웃

---
+++ myBlog/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/',require('./routes/main'));
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port,() => {
+    console.log(`App listening on port ${port}`);
+});
---
+++ myBlog/routes/main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const mainLayout = "../views/layouts/main.ejs";
+
+router.get(['/','/home'],(req,res) => {
+    const locals = {
+        title: 'Home'
+    }
+    res.render('index', {locals, layout: mainLayout});
+});
+
+router.get('/about',(req,res) => {
+    const locals = {
+        title: 'about'
+    }
+    res.render('about', {locals, layout: mainLayout});
+});
+ 
+module.exports = router;
