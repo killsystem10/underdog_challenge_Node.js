fe43edbd3a18278cd27d527bc64018653e451f03
killsystem10 <killsystem10@outlook.com>
2025-05-04 10:51:36 +0900

    Section 5 04. 진행중

--- myBlog/app.js
+++ myBlog/app.js
@@ -15,6 +15,9 @@ app.set('views','./views');
 
 app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.listen(port,() => {
     console.log(`App listening on port ${port}`);
 });
--- myBlog/routes/admin.js
+++ myBlog/routes/admin.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const adminLayout = '../views/layouts/admin';
 const nologoutadmin = '../views/layouts/admin-nologout';
+const asynchandler = require('express-async-handler');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 router.get('/admin',(req,res) => {
     const locals = {
@@ -9,5 +12,19 @@ router.get('/admin',(req,res) => {
     }
     res.render('admin/index', {locals, layout: nologoutadmin}); 
 });
+
+router.get('/register', asynchandler(async (req,res) => {
+    res.render('admin/index', {layout: nologoutadmin})
+}));
+
+router.post('/register', asynchandler(async (req,res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create( {
+        username: req.body.username,
+        password: hashedPassword
+    });
+    res.json(`user created: ${user}`);
+})
+);
 
 module.exports = router;
